38acd94de6c29e0961f97fd8286ce9b3980810da
baui <bauer.mart>
2017-01-24 14:39:41 +0100

    Der Stundenplan wird mit einem festem Modul angezeigt implementiert ist: - Darstellung - Logik für Buttons - Darstellung für unterschiedliche Display Größen - Fehlermeldung bei über-/unterschreiten der Woche
    Noch nicht implementiert ist:
    - Markierung des gerade stattfindenden Moduls
    - Anzeige der Lage des Raumes von einem Modul

---
+++ app/src/main/java/s1eustei/eah/eah_app/Abos.java
@@ -0,0 +1,15 @@
+package s1eustei.eah.eah_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Martin Bauer on 24.01.2017.
+ */
+
+public class Abos extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/s1eustei/eah/eah_app/Stundenplan.java
+++ app/src/main/java/s1eustei/eah/eah_app/Stundenplan.java
@@ -1,9 +1,13 @@
 package s1eustei.eah.eah_app;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -16,6 +20,8 @@ import java.util.Date;
 public class Stundenplan extends AppCompatActivity {
 
     LinearLayout _linearLayout;
+    Button _ButtonNextWeek, _ButtonPreviosWeek, _ButtonEdit;
+    int _week = 1;
     String _BackgroundColor = "#e7ffff";
     String _ColorActivModul = "#009696";
     String _ColorButton = "#FF56FAB8";
@@ -59,7 +65,12 @@ public class Stundenplan extends AppCompatActivity {
         extLayout.addView(ButtonPreviosWeek);
         extLayout.addView(ButtonEdit);
         setContentView(extLayout);
+
+        //get access to objects
         _linearLayout = linearLayout;
+        _ButtonNextWeek = ButtonNextWeek;
+        _ButtonPreviosWeek = ButtonPreviosWeek;
+        _ButtonEdit = ButtonEdit;
     }
 
     private void addWeek(int week,String wocheAnfangEnde) {
@@ -114,6 +125,7 @@ public class Stundenplan extends AppCompatActivity {
         column2.setText(modul + "\n" + dozent + "\n" + raum);
         column3.setText(ende);
 
+        /*
         //marks the current Modules
         start = start.replaceAll(":","");
         if (start.charAt(0) == '0'){
@@ -133,7 +145,8 @@ public class Stundenplan extends AppCompatActivity {
 
         //datum = datum.replaceAll(".","");
         //datum.replaceAll("0","");
-        
+
+
 
         timeClock = String.valueOf(calendar.getHours()) + String.valueOf(calendar.getMinutes());
 
@@ -154,7 +167,7 @@ public class Stundenplan extends AppCompatActivity {
         if (datumCurrent == datumModul)
             if (zeitCurrent >= startzeitModul || zeitCurrent <= endzeitModul)
                 tableRow.setBackgroundColor(Color.parseColor(_ColorActivModul));
-
+        */
         //add to ScrollView;
         tableRow.addView(column1);
         tableRow.addView(column2);
@@ -170,9 +183,113 @@ public class Stundenplan extends AppCompatActivity {
         _linearLayout.addView(textView);
     }
 
-    private void getModuleforDay(String day){
-        addModul("00:01","aslkfdjaö","adlfk","dlfkö","23:00","23.01.217");
-        addModul("00:02","ET(BA)Computergrafik/V/Ü/P/01.5", "Braungart, Herbst", "05.02.38", "23:59", "22.01.217");
+    private void getStundenplan(){
+        int counter, id;
+        String wocheAnfangEnde;
+        String Day[] = {"Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Sonnabend"};
+
+        //create ScrollView and Buttons
+        createView();
+        //wocheAnfangEnde = Datenbank.getWocheAnfangEnde(week);
+        addWeek(_week,"17.10.2016 - 22.10.2016");
+        for (counter = 0; counter <= 5; counter++) {
+            id = 1;
+            addDay(Day[counter]);
+            addModul("00:02", "ET(BA)Computergrafik/V/Ü/P/01.5", "Braungart, Herbst", "05.02.38", "23:59", "22012017");
+            /*
+            Datenbank modul = Datenbank.getNextModul(week,counter,id);
+            while (modul != null){
+                addModul(modul);
+                //click Listener
+                id++;
+            }*/
+
+        }
+    }
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        getStundenplan();
+
+        _ButtonPreviosWeek.setOnClickListener(OnBtnPreviosWeekListener);
+        _ButtonNextWeek.setOnClickListener(OnBtnNextWeekListener);
+        _ButtonEdit.setOnClickListener(OnBtnEditListener);
+
+    }
+
+    private View.OnClickListener OnBtnPreviosWeekListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view)
+        {
+            if (_week > 1) {
+                _week = _week - 1;
+                getStundenplan();
+                _ButtonPreviosWeek.setOnClickListener(OnBtnPreviosWeekListener);
+                _ButtonNextWeek.setOnClickListener(OnBtnNextWeekListener);
+                _ButtonEdit.setOnClickListener(OnBtnEditListener);
+            } else AllertWocheRunter();
+        }
+    };
+
+    private View.OnClickListener OnBtnNextWeekListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view)
+        {
+            if (_week < 17) {
+                _week = _week + 1;
+                getStundenplan();
+                _ButtonPreviosWeek.setOnClickListener(OnBtnPreviosWeekListener);
+                _ButtonNextWeek.setOnClickListener(OnBtnNextWeekListener);
+                _ButtonEdit.setOnClickListener(OnBtnEditListener);
+            } else AllertWocheHoch();
+        }
+    };
+
+    private View.OnClickListener OnBtnEditListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view)
+        {
+            Intent intent = new Intent(Stundenplan.this, Abos.class);
+            startActivity(intent);
+        }
+    };
+
+    private void AllertWocheHoch (){
+        AlertDialog.Builder msg = new AlertDialog.Builder(this);
+        msg.setTitle("Fehlermeldung");
+        msg.setMessage("Sie befinden sich bereits in der letzten Woche!!!");
+        msg.setCancelable(true);
+        AlertDialog msgDialog = msg.create();
+        msgDialog.show();
+    };
+
+    private void AllertWocheRunter(){
+        AlertDialog.Builder msg = new AlertDialog.Builder(this);
+        msg.setTitle("Fehlermeldung");
+        msg.setMessage("Sie befinden sich bereits in der ersten Woche!!!");
+        msg.setCancelable(true);
+        AlertDialog msgDialog = msg.create();
+        msgDialog.show();
+    };
+
+
+    protected void onSaveInstanceState(Bundle saving) {
+        //save the variable _week, when app is going on pause
+        saving.putInt("_week",_week);
+        super.onSaveInstanceState(saving);
+    }
+
+    protected void onRestoreInstanceState(Bundle restoring) {
+        //restore the variable _week when the app is onStart()
+        _week = restoring.getInt("_week",1);
+        super.onRestoreInstanceState(restoring);
+        getStundenplan();
+        _ButtonPreviosWeek.setOnClickListener(OnBtnPreviosWeekListener);
+        _ButtonNextWeek.setOnClickListener(OnBtnNextWeekListener);
+        _ButtonEdit.setOnClickListener(OnBtnEditListener);
     }
 
 }
