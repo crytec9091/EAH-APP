5c5ce663e9abf2a22ecd27d4f62d5566b6e4d78b
franz <toxxxsys@gmail.com>
2017-01-31 08:52:16 +0100

    löse abhänigkeiten auf

--- app/src/main/java/s1eustei/eah/eah_app/ParseKalender.java
+++ app/src/main/java/s1eustei/eah/eah_app/ParseKalender.java
@@ -13,15 +13,19 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import lern
+import java.sql.Date;
+import java.util.List;
+
 public class ParseKalender {
     private org.jsoup.nodes.Document htmlSite;
     private boolean success;
     private  org.jsoup.select.Elements tbody;
+    private lernModul store;
+    private List<lernModul> modulList;
 
     public void getHtml(String uri){
         try{
-            htmlSite=Jsoup.connect(uri);
+            htmlSite=Jsoup.connect(uri).get();
             success=true;
         }
         catch(IOException e){
@@ -29,7 +33,11 @@ public class ParseKalender {
         }
 
     }
-    public Parse(){
+    public void set_string(String s){
+        htmlSite=Jsoup.parse(s);
+        success=true;
+    }
+    public void Parse(){
         tbody = htmlSite.select("tbody");
         for(org.jsoup.nodes.Element table :tbody){
             org.jsoup.select.Elements trs = table.children();
@@ -42,7 +50,18 @@ public class ParseKalender {
                 String Raum=tds.get(4).text();
                 String Wochen= tds.get(5).text();
                 String Beginn=tds.get(6).text();
+                store = new lernModul();
+                store.Name=Name;
+                store.Dozent=Dozent;
+                store.Raum=Raum;
+                store.Sichtbar=true;
+                store.Anfang= Date.valueOf(Beginn);
+                store.Ende=Date.valueOf(Ende);
+                modulList.add(store);
             }
         }
+    }
+    public List<lernModul> get_modul_list(){
+        return modulList;
     }
 }
